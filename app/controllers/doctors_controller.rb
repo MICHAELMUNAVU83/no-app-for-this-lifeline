7593ec0cd0d75491405ef980fb6c8898957688f5
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-01 18:24:48 +0300

    styled select

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -14,11 +14,20 @@ class DoctorsController < ApplicationController
     @diabetes = PreExistingCondition.where(diabetes: true).count
     @hypertension = PreExistingCondition.where(hypertension: true).count
     @asthma = PreExistingCondition.where(asthma: true).count
-    @total = @diabetes + @hypertension + @asthma
+    @total = @diabetes + @hypertension + @asthma 
 
-    @diabetes_percentage = @diabetes.to_f / @total * 100
-    @hypertension_percentage = @hypertension.to_f / @total * 100
-    @asthma_percentage = @asthma.to_f / @total * 100
+    if @diabetes.zero? && @hypertension.zero? && @asthma.zero?
+
+      @diabetes_percentage = 0
+      @hypertension_percentage = 0
+      @asthma_percentage = 0
+    else
+
+    @diabetes_percentage = (@diabetes.to_f / @total * 100).round(2)
+    @hypertension_percentage = (@hypertension.to_f / @total * 100).round(2)
+    @asthma_percentage = (@asthma.to_f / @total * 100).round(2)
+    end
+
   end
 
   def recent_patients
