d72fbc5da1a28c0d5a27f4b77fda8b8bbb1aa1fd
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-01 12:56:01 +0300

    added next of kins

---
+++ app/controllers/next_of_kins_controller.rb
@@ -0,0 +1,64 @@
+class NextOfKinsController < ApplicationController
+  before_action :set_patient
+  before_action :set_next_of_kin, only: [:show, :edit, :update, :destroy]
+
+  # GET patients/1/next_of_kins
+  def index
+    @next_of_kins = @patient.next_of_kins
+  end
+
+  # GET patients/1/next_of_kins/1
+  def show
+  end
+
+  # GET patients/1/next_of_kins/new
+  def new
+    @next_of_kin = @patient.next_of_kins.build
+  end
+
+  # GET patients/1/next_of_kins/1/edit
+  def edit
+  end
+
+  # POST patients/1/next_of_kins
+  def create
+    @next_of_kin = @patient.next_of_kins.build(next_of_kin_params)
+
+    if @next_of_kin.save
+      redirect_to([@next_of_kin.patient, @next_of_kin], notice: 'Next of kin was successfully created.')
+    else
+      render action: 'new'
+    end
+  end
+
+  # PUT patients/1/next_of_kins/1
+  def update
+    if @next_of_kin.update_attributes(next_of_kin_params)
+      redirect_to([@next_of_kin.patient, @next_of_kin], notice: 'Next of kin was successfully updated.')
+    else
+      render action: 'edit'
+    end
+  end
+
+  # DELETE patients/1/next_of_kins/1
+  def destroy
+    @next_of_kin.destroy
+
+    redirect_to patient_next_of_kins_url(@patient)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_patient
+      @patient = Patient.find(params[:patient_id])
+    end
+
+    def set_next_of_kin
+      @next_of_kin = @patient.next_of_kins.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def next_of_kin_params
+      params.require(:next_of_kin).permit(:name, :phone_number, :relation, :patient_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_065605) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_095150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_065605) do
     t.datetime "updated_at", null: false
     t.integer "pre_existing_condition_count", default: 0
     t.date "date_of_birth"
+    t.string "blood_group"
+    t.boolean "dnr"
+    t.boolean "dni"
     t.index ["doctor_id"], name: "index_patients_on_doctor_id"
   end
 
